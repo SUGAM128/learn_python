4d95905340ced52951579e3e3640d91ccb423470
SUGAM128 <sugamgautam.128@gmail.com>
2025-06-01 12:47:14 +0545

    upto registration

--- django_project/app/urls.py
+++ django_project/app/urls.py
@@ -2,6 +2,7 @@ from django.urls  import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="index"),
-    path('counter',views.counter,name='counter')
+    path('',views.index,name='index'),
+    path('counter',views.counter,name='counter'),
+    path('register',views.register, name='register')
 ]
